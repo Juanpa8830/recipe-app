5afdd85f2ed127226b3c4c490d8b4b7fdd323fed
Juanpa8830 <jp3_4@hotmail.com>
2023-04-27 15:17:55 -0500

    Add Recipe_food model

---
+++ app/models/recipe_food.rb
@@ -0,0 +1,7 @@
+class RecipeFood < ApplicationRecord
+belongs_to :recipe, class_name: 'Recipe'
+belongs_to :food, class_name: 'Food'
+
+validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+end
