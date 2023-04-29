e3a80c03ce4c6e8327e3f73864fc6b3ea7666959
Juanpa8830 <jp3_4@hotmail.com>
2023-04-29 13:58:39 -0500

    Add missing setup

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module RecipeApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.action_mailer.default_url_options = { host: 'example.com' }
+
   end
 end
